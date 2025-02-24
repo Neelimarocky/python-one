5fdb043a3e18f06906894161a0c44aed7762bf9b
Neelimarocky <telakapallineelima@gmail.com>
2025-02-24 21:09:35 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,35 @@
+#integer(+,-,*,%,**,//)
+a=int(input("enter the a value:"))
+b=int(input("enter b value:"))
+result=a+b
+print(result)
+#strings
+string="hello world"
+string2="hai"
+string3="welcome!"
+print(string)
+print(string2)
+print(string3)
+#slicing
+my = "Hello, World"
+sliced = my[0:5]
+print(sliced) 
+my_string="hello world"
+last = my_string[-1]
+print(last)
+last1 = my_string[-5:]
+print(last1)
+reversed_string = my_string[::-1]
+print(reversed_string) 
+#indexing
+my_list = [10, 20, 30, 40, 50]
+# Positive Indexing
+print(my_list[0])  
+print(my_list[3]) 
+# Negative Indexing
+print(my_list[-1]) 
+print(my_list[-3])  
+
+
+
+
